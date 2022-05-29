17dca45151ba468d6450bccee91993afb3b1aa02
Oskar Gomza <oskargomza54@gmail.com>
2022-05-29 22:25:29 +0200

    Progress

--- caesar.cpp
+++ caesar.cpp
@@ -6,40 +6,54 @@ int main(int argc, char *argv[])
 {
     char c;
     int mov = std::stoi(argv[1]);
-    if(mov>26 || mov<-26) {
-        mov%=26;
+    if (mov > 26 || mov < -26)
+    {
+        mov %= 26;
     }
     std::ifstream input(argv[2]);
     std::ofstream output(argv[3]);
 
-       while (input.get(c)) {
-          if(c>=65 && c<=90) {
-                if(int(c+mov)>90) {
-                    output << char(c+mov-26);
+    while (input.get(c))
+    {
+        if (c >= 65 && c <= 90)
+        {
+            if (int(c + mov) > 90)
+            {
+                output << char(c + mov - 26);
+            }
+            else
+            {
+                if (int(c + mov) < 65)
+                {
+                    output << char(c + mov + 26);
                 }
-                else {
-                    if (int(c+mov)<65) {
-                        output << char(c+mov+26);
-                    }
-                    else output << char(c+mov);
+                else
+                    output << char(c + mov);
+            }
+        }
+        else
+        {
+            if (c >= 97 && c <= 122)
+            {
+                if (int(c + mov) > 122)
+                {
+                    output << char(c + mov - 26);
                 }
-          }
-          else {
-                if (c>=97 && c<=122) {
-                    if(int(c+mov)>122) {
-                        output << char(c+mov-26);
+                else
+                {
+                    if (int(c + mov) < 97)
+                    {
+                        output << char(c + mov + 26);
                     }
-                    else {
-                        if (int(c+mov)<97) {
-                            output << char(c+mov+26);
-                        }
-                        else output << char(c+mov);
-                    }
-              }
-              else output << c;
-          }
-       }
+                    else
+                        output << char(c + mov);
+                }
+            }
+            else
+                output << c;
+        }
+    }
 
-       input.close();
-       output.close();
+    input.close();
+    output.close();
 }
