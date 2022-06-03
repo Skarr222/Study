226823253f145308e78f82115db8ff9eb6153eb2
Oskar Gomza <oskargomza54@gmail.com>
2022-06-03 19:16:01 +0200

    update

--- C++/Study1.cpp
+++ C++/Study1.cpp
@@ -8,6 +8,15 @@ int main()
         double number;
         std::cout << "Enter positive number: ";
         std::cin >> number;
+        if (number < 0.)
+        {
+            continue;
+        }
+        if (number == 0.)
+        {
+            break;
+        }
         std::cout << "Square root: " << std::sqrt(number) << std::endl;
     }
+    std::cout << "Thank you!" << std::endl;
 }
