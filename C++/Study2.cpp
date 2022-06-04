e56c3d0f109b7a9e1c8bf26fa0bc2fc8dce03125
Oskar Gomza <oskargomza54@gmail.com>
2022-06-04 12:19:24 +0200

    stud

--- C++/Study2.cpp
+++ C++/Study2.cpp
@@ -1,2 +1,14 @@
 #include <iostream>
 #include <cmath>
+
+int g = 10;
+int &function(){
+    return g;
+}
+
+int main()
+{
+    std::cout << g << " " << function() << std::endl;
+    function() = 12;
+    std::cout << g << " " << function() << std::endl;
+}
