ce5ac2b93a751f2887dbc5e2afb1e2837b6606ed
Oskar Gomza <oskargomza54@gmail.com>
2022-06-30 20:35:28 +0200

    execises from lab3

--- C++/Lab3.cpp
+++ C++/Lab3.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <vector>
+
+/*
 void clock(double &s, double &l, int h, int m)
 {
     double s = (h % 12) * 30. + m / 2.;
@@ -13,4 +16,13 @@ int main()
     clock(s, l, h, m);
 
     std::cout << s << " " << l << std::endl;
+}*/
+
+int main()
+{
+    std::vector<int> v;
+    for (int x; std::cin >> x;)
+    {
+        v.push_back(x)
+    }
 }
