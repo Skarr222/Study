4170b6863c752419c7093baeb9a0d1f83b4aa34e
Oskar Gomza <oskargomza54@gmail.com>
2022-06-30 13:28:19 +0200

    lab2 execise

---
+++ C++/Lab2.cpp
@@ -0,0 +1,19 @@
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+int main()
+{
+    std::srand(std::time(nullptr));
+    for (int i = 0; i < 10; ++i)
+    {
+        int a = 1 + std::rand() % 10;
+        int b = 1 + std::rand() % 10;
+        int u;
+        do
+        {
+            std::cout << a << "  " << b << "  ";
+            std::cin >> u;
+        } while (u != a * b);
+    }
+}
