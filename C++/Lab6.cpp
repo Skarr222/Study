d4115b858bf8fce3766c227074c469323516070c
Oskar Gomza <oskargomza54@gmail.com>
2022-07-01 14:02:53 +0200

    execises from lab6

--- C++/Lab6.cpp
+++ C++/Lab6.cpp
@@ -4,17 +4,17 @@
 
 void selection_sort(std::vector<int> &v)
 {
-    for (int j = 0; j < v.size(); ++j)
+    for (std::vector<int>::iterator j = v.begin(); j < v.end(); ++j)
     {
-        int m = j;
-        for (int i = j; ++i < v.size();)
+        std::vector<int>::iterator m = j;
+        for (std::vector<int>::iterator i = j; ++i < v.end();)
         {
-            if (v[i] < v[m])
+            if (*i < *m)
             {
                 m = i;
             }
         }
-        std::swap(v[j], v[m]);
+        std::swap(*j, *m);
     }
 }
 
@@ -22,12 +22,6 @@ int main()
 {
     std::vector<int> v{13, -2, 21, 5, -8, 5, 7, -10};
     selection_sort(v);
-    for (int i = 0; i < v.size(); ++i)
-    {
-        std::cout << v[i] << " ";
-    }
-    std::cout << std::endl;
-
     for (std::vector<int>::iterator i = v.begin; i < v.end(); ++i)
     {
         std::cout << *i << " ";
