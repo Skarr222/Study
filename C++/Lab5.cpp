17bf2e7e1cb24cd225b84738956118fb3ecb8322
Oskar Gomza <oskargomza54@gmail.com>
2022-07-01 11:29:20 +0200

    Execises from lab5

--- C++/Lab5.cpp
+++ C++/Lab5.cpp
@@ -7,9 +7,11 @@ int main()
 
     while (std::getline(std::cin, s))
     {
-        int j = s.find_first_not_of(" \t");
+        int j = s.find_first_not_of(" \t"); /*Wskazuje ostatni indeks w łańcuchu niebędący spacją lub tabulatorem
+        s.erase(j + 1); /*Usuwa spacje i tabulatory za ostatnim znakiem*/
+
         /*
-      for (int i = 0; i < s.size(); ++i)
+      for (int i = 0; i < s.size(); ++i) Jest to funkcja find first not of wykonana za pomocą podstawowych funckji języka cpp
       {
           if (s[i] != ' ' && s[i] != '\t')
           {
