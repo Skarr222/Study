59b9d338cbe5fb15b7d809af941f4a3c956ec99e
Oskar Gomza <oskargomza54@gmail.com>
2022-07-01 15:56:14 +0200

    execises from lab7

--- C++/Lab7.cpp
+++ C++/Lab7.cpp
@@ -9,18 +9,24 @@ bool blank(const std::string &s)
 }
 void trim(std::string &s)
 {
-    auto i = std::find_if(s.begin(), s.end(), [](char c)
-                          { return !(std::isspace(c)); });
-    s.erase(s.begin(), i);
+    auto i1 = std::find_if(s.begin(), s.end(), [](char c)
+                           { return !(std::isspace(c)); });
+    s.erase(s.begin(), i1);
+
+    auto i2 = std::find_if(s.rbegin(), s.rend(), [](char c)
+                           { return !(std::isspace(c)); });
+    s.erase(i2.base(), s.end());
+}
+
+void contains(const std::string &s, const std::string &p)
+{
+    auto i = std::search(s.begin(), s.end(), p.begin(), p.end());
 }
 
 int main()
 {
     std::string s = "   Hello world!     ";
-    for (std::string::iterator i = s.begin(); i < s.end(); ++i)
-    {
-        std::cout << *i;
-    }
     trim(s);
-    std::cout << std::endl;
+    bool result = contains(s, "lo");
+    std::cout << std::boolalpha << result << std::endl;
 }
