d6e824d2ef8c660ac4bc5ddb518fd98e6042a8da
Oskar Gomza <oskargomza54@gmail.com>
2022-07-01 11:25:02 +0200

    Execises from lab5

--- C++/Lab5.cpp
+++ C++/Lab5.cpp
@@ -1,10 +1,23 @@
 #include <iostream>
+#include <string>
 
 int main()
 {
     std::string s;
-    while (std::cin >> s)
+
+    while (std::getline(std::cin, s))
     {
-        std::cout << s << std::endl;
+        int j = s.find_first_not_of(" \t");
+        /*
+      for (int i = 0; i < s.size(); ++i)
+      {
+          if (s[i] != ' ' && s[i] != '\t')
+          {
+              j = i;
+          }
+      }
+      */
+        std::cout << j << std::endl;
+        std::cout << s << "|" << std::endl;
     }
 }
