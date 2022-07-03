8cfe150ee7ca1cee0ae5b568839d2d76e954fc85
Oskar Gomza <oskargomza54@gmail.com>
2022-07-03 16:12:22 +0200

    homework

--- C++/Remove_If.cpp
+++ C++/Remove_If.cpp
@@ -2,4 +2,25 @@
 #include <functional>
 #include <vector>
 
-std::
+std::vector<int>::iterator remove_if(std::vector<int>::iterator x, std::vector<int>::iterator y, [](int element)
+                                     { return bool element; })
+{
+    auto = m;
+    for (auto i = x; i < y; ++i)
+    {
+        if (i <=)
+    }
+}
+
+int main()
+{
+    std::vector<int> vector{-7, 5, 2, 2, 11, 2, 3};
+    auto result = remove_if(vector.begin(), vector.end(),
+                            [](int element)
+                            { return element < 3; });
+    for (auto iterator = vector.cbegin(); iterator < result;)
+    {
+        std::cout << *iterator++ << " ";
+    }
+    std::cout << std::endl;
+}
