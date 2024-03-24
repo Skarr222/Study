1608e266fab6426e5e8f9c3621a6e5e79d69a250
Skarr222 <oskargomza54@gmail.com>
2024-03-24 14:35:30 +0100

    feat(w6): creating new class to incorporate virtual function and to avoid code repeating

--- cpp/Study-NLU/w6/main.cpp
+++ cpp/Study-NLU/w6/main.cpp
@@ -14,26 +14,31 @@ public:
 class Pracownik : public Osoba {
     private:
     std::string stanowisko;
-    std::string placa;
     public:
-    Pracownik(std::string imie, std::string nazwisko, std::string stanowisko, std::string placa) 
-    : Osoba(imie, nazwisko), stanowisko(stanowisko), placa(placa) {}
+    Pracownik(std::string imie, std::string nazwisko, std::string stanowisko) 
+    : Osoba(imie, nazwisko), stanowisko(stanowisko) {}
     void Drukuj() const {
         Osoba::Drukuj();
         std::cout << "Stanowisko: " << stanowisko << std::endl;
-        std::cout << "Placa: " << placa << std::endl;
+    }
+    double DochodyRoczne (){
+        return 0;
     }
 };
 
 class PracownikEtatowy : public Pracownik {
     private:
-    std::string placaBrutto;
+    double placaBrutto;
     public:
-    PracownikEtatowy(std::string imie, std::string nazwisko, std::string stanowisko, std::string placa, std::string placaBrutto) 
-    : Pracownik(imie, nazwisko, stanowisko, placa), placaBrutto(placaBrutto) {}
+    PracownikEtatowy(std::string imie, std::string nazwisko, std::string stanowisko, double placaBrutto) 
+    : Pracownik(imie, nazwisko, stanowisko), placaBrutto(placaBrutto) {}
     void Drukuj() const {
         Pracownik::Drukuj();
         std::cout << "Płaca Brutto: " << placaBrutto << std::endl;
+    }
+
+    double DochodyRoczne (){
+        return placaBrutto * 12;
     }
 };
 class PracownikGodzinowy : public Pracownik{
@@ -41,34 +46,57 @@ class PracownikGodzinowy : public Pracownik{
     double stawkaGodzinowa;
     double liczbaGodzin;
     public:
-    PracownikGodzinowy(std::string imie, std::string nazwisko, std::string stanowisko, std::string placa, double stawkaGodzinowa, double liczbaGodzin) 
-    : Pracownik(imie, nazwisko, stanowisko, placa), stawkaGodzinowa(stawkaGodzinowa), liczbaGodzin(liczbaGodzin) {}
+    PracownikGodzinowy(std::string imie, std::string nazwisko, std::string stanowisko, double stawkaGodzinowa, double liczbaGodzin) 
+    : Pracownik(imie, nazwisko, stanowisko), stawkaGodzinowa(stawkaGodzinowa), liczbaGodzin(liczbaGodzin) {}
     void Drukuj() const {
         Pracownik::Drukuj();
         std::cout << "Stawka Godzinowa: " << stawkaGodzinowa << std::endl;
         std::cout << "Liczba Godzin: " << liczbaGodzin << std::endl;
     }
-    double Wynagrodzenie() const {
-        return stawkaGodzinowa * liczbaGodzin;
+    double DochodyRoczne (){
+        return liczbaGodzin * stawkaGodzinowa;
+    }
+};
+class Ksiegowosc{
+    public:
+    void  DrukujPIT (Pracownik& pracownik){
+        std::cout << "Pit 11: " << pracownik.DochodyRoczne() << std::endl;
+        std::cout << "Dane Podatnika: "  << std::endl;
+        pracownik.Drukuj();
+        std::cout << "Dochody roczne podatnika: " << pracownik.DochodyRoczne() << std::endl;
     }
 };
 
 int main() {
     //* wskaźnik na osobe wskazuje na osobe
-    Osoba osoba("Jan", "Kowalski");
-    Osoba *wsk1 = &osoba;
-    wsk1->Drukuj();
+    //* Osoba osoba("Jan", "Kowalski");
+    //* Osoba *wsk1 = &osoba;
+    //* wsk1->Drukuj();
 
     //* wskaźnik na osobe wskazuje na pracownika
-    Pracownik pracownik("Jan", "Kowalski", "Informatyk", "5000");
-    Osoba *wsk2 = &pracownik;
+    //*  Osoba *wsk2 = &pracownik;
+    //*  wsk2->Drukuj();
 
-    //* wskaźnik na pracownika wskazuje na osobe 
-    //! BŁĄD WSKAŹNIK PRACOWNIKA NIE MOŻE WSKAZYWAĆ NA OSOBE ponieważ 
-    //! pracownik ma więcej pól niż osoba więce nie możemy wskazać 
-    //! na osobe ponieważ może nam brakować danych
-    // Pracownik *wsk3 = &osoba
-    // wsk3->Drukuj();
+    /*
+    * wskaźnik na pracownika wskazuje na osobe
+    ! BŁĄD WSKAŹNIK PRACOWNIKA NIE MOŻE WSKAZYWAĆ NA OSOBE ponieważ
+    ! pracownik ma więcej pól niż osoba więce nie możemy wskazać 
+    ! na osobe ponieważ może nam brakować danych
+    ! Pracownik *wsk3 = &osoba
+    ! wsk3->Drukuj();
+    */  
+   
+    Pracownik pracownik("Jan", "Pracownik", "Informatyk");
+    std::cout << "Pracownik:" << std::endl;
+    pracownik.Drukuj();
+
+    PracownikEtatowy pracownikEtatowy("Jan", "Etatowy", "Kierownik", 6000);
+    std::cout << "Pracownik Etatowy:" << std::endl;
+    pracownikEtatowy.Drukuj();
+
+    PracownikGodzinowy pracownikGodzinowy("Jan", "Godzinowy", "Kierownik", 50, 100);
+    std::cout << "Pracownik Godzinowy:" << std::endl;
+    pracownikGodzinowy.Drukuj();
 
     return 0;
 }
