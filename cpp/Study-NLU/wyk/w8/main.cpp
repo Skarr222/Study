8bf0ec5aacdfdaf89a1e35370586808fab681dd6
Skarr222 <oskargomza54@gmail.com>
2024-06-02 14:11:52 +0200

    quick comments

--- cpp/Study-NLU/wyk/w8/main.cpp
+++ cpp/Study-NLU/wyk/w8/main.cpp
@@ -1,63 +1,63 @@
-#include <iostream>
-using namespace std;
-class Rachunek
-{
-private:
-    string numer;
-    double saldo;
+// #include <iostream>
+// using namespace std;
+// class Rachunek
+// {
+// private:
+//     string numer;
+//     double saldo;
 
-public:
-    Rachunek()
-    {
-    }
-    Rachunek(string numer, double saldo)
-        : numer(numer), saldo(saldo)
-    {
-    }
-    void Drukuj() const
-};
-class klient
-{
-private:
-    string imie,
-        nazwisko,
-        numer public : = "" klient() = "" klient(string = "" imie, string = "" nazwisko, string = "" numer) = "" : imie(imie), nazwisko(nazwisko), numer(numer) = "" void = "" drukuj() = "" const = "" cout << endl
-                                                                                                                                                                                                             << endl
-                                                                                                                                                                                                             << "klient";
-    cout << endl
-         << "imie:="
-         << imie;
-    cout << endl
-         << "nazwisko:="
-            " "
-         << nazwisko;
-    zbiorr = "" klasa = "" bedzie = "" przechwowywala = "" wszystkie = "" rachunki = "" banku = "" w = "" tablicy = "" rachunek = "" * tab;
-    to = "" jest = "" wskaznaik = "" do = "" pierwszego = "" elementu = "" rachunkow = "" int = "" liczbarachunkow;
-    zbiorr() = "" tab = "new" rachunek[20];
-    tworzymy = "" tablice = "" rachunków = ""(pusta) = "" o = "" max = "" rozmiarze = "" liczbarachunkow = "0;" dodajrachunek(rachunek = "" nowy) = "" tab[liczbarachunkow] = "nowy;" liczbarachunkow++;="" drukujwszystkie()="" for(int="" i="0;i<liczbaRachunkow;i++)" tab[i].drukuj();="" tab[i]="" konkretny="" rachunek,="" zatem="" wywolujemy="" metode="" drukuj="" dla="" konkretnego="" rahcunkui="" powinnien="" być="" dopisany="" też="" destruktor="" kasujacy="" zbiork="" wszystkich="" klientow="" liczbaklientow;="" zbiork()="" klient[20];="" liczbaklientow="0;" dodajklienta(klient="" tab[liczbaklientow]="nowy;" liczbaklientow++;="" klient,="" klienta="" main()="" cout<<"przykład="" szablonu="" klas";="" zbiorr;="" r1("1",1);="" zbiorr.dodajrachunek(r1);="" r2("2",2);="" zbiorr.dodajrachunek(r2);="" r3("3",3);="" zbiorr.dodajrachunek(r3);="" r4("4",4);="" zbiorr.dodajrachunek(r4);="" r5("5",5);="" zbiorr.dodajrachunek(r5);="" zbiorr.drukujwszystkie();="" zbiork;="" k1("adam1","kot1","1");="" zbiork.dodajklienta(k1);="" k2("adam2","kot2","2");="" zbiork.dodajklienta(k2);="" k3("adam3","kot3","3");="" zbiork.dodajklienta(k3);="" k4("adam4","kot4","4");="" zbiork.dodajklienta(k4);="" k5("adam5","kot5","5");="" zbiork.dodajklienta(k5);="" zbiork.drukujwszystkie();="" return="" 0;=""
-} <= "" p = "" >
-            < / endl
-                  << endl
-                  << "rachunek";
->
+// public:
+//     Rachunek()
+//     {
+//     }
+//     Rachunek(string numer, double saldo)
+//         : numer(numer), saldo(saldo)
+//     {
+//     }
+//     void Drukuj() const
+// };
+// class klient
+// {
+// private:
+//     string imie,
+//         nazwisko,
+//         numer public : = "" klient() = "" klient(string = "" imie, string = "" nazwisko, string = "" numer) = "" : imie(imie), nazwisko(nazwisko), numer(numer) = "" void = "" drukuj() = "" const = "" cout << endl
+//                                                                                                                                                                                                              << endl
+//                                                                                                                                                                                                              << "klient";
+//     cout << endl
+//          << "imie:="
+//          << imie;
+//     cout << endl
+//          << "nazwisko:="
+//             " "
+//          << nazwisko;
+//     zbiorr = "" klasa = "" bedzie = "" przechwowywala = "" wszystkie = "" rachunki = "" banku = "" w = "" tablicy = "" rachunek = "" * tab;
+//     to = "" jest = "" wskaznaik = "" do = "" pierwszego = "" elementu = "" rachunkow = "" int = "" liczbarachunkow;
+//     zbiorr() = "" tab = "new" rachunek[20];
+//     tworzymy = "" tablice = "" rachunków = ""(pusta) = "" o = "" max = "" rozmiarze = "" liczbarachunkow = "0;" dodajrachunek(rachunek = "" nowy) = "" tab[liczbarachunkow] = "nowy;" liczbarachunkow++;="" drukujwszystkie()="" for(int="" i="0;i<liczbaRachunkow;i++)" tab[i].drukuj();="" tab[i]="" konkretny="" rachunek,="" zatem="" wywolujemy="" metode="" drukuj="" dla="" konkretnego="" rahcunkui="" powinnien="" być="" dopisany="" też="" destruktor="" kasujacy="" zbiork="" wszystkich="" klientow="" liczbaklientow;="" zbiork()="" klient[20];="" liczbaklientow="0;" dodajklienta(klient="" tab[liczbaklientow]="nowy;" liczbaklientow++;="" klient,="" klienta="" main()="" cout<<"przykład="" szablonu="" klas";="" zbiorr;="" r1("1",1);="" zbiorr.dodajrachunek(r1);="" r2("2",2);="" zbiorr.dodajrachunek(r2);="" r3("3",3);="" zbiorr.dodajrachunek(r3);="" r4("4",4);="" zbiorr.dodajrachunek(r4);="" r5("5",5);="" zbiorr.dodajrachunek(r5);="" zbiorr.drukujwszystkie();="" zbiork;="" k1("adam1","kot1","1");="" zbiork.dodajklienta(k1);="" k2("adam2","kot2","2");="" zbiork.dodajklienta(k2);="" k3("adam3","kot3","3");="" zbiork.dodajklienta(k3);="" k4("adam4","kot4","4");="" zbiork.dodajklienta(k4);="" k5("adam5","kot5","5");="" zbiork.dodajklienta(k5);="" zbiork.drukujwszystkie();="" return="" 0;=""
+// } <= "" p = "" >
+//             < / endl
+//                   << endl
+//                   << "rachunek";
+// >
 
-    int main()
-{
-    Rachunek rachunek();
-    return 0;
-}
-< endl << endl
-       << "rachunek";
-cout << endl
-     << "numer:="
-        " "
-     << numer;
-= "" cout << endl
-          << "saldo:="
-             " "
-          << saldo;
-= ""
-}
-= ""
-}
-;
+//     int main()
+// {
+//     Rachunek rachunek();
+//     return 0;
+// }
+// < endl << endl
+//        << "rachunek";
+// cout << endl
+//      << "numer:="
+//         " "
+//      << numer;
+// = "" cout << endl
+//           << "saldo:="
+//              " "
+//           << saldo;
+// = ""
+// }
+// = ""
+// }
+// ;
