c0e301924469314e6a1f684ba5b1e8c411f8a458
Skarr222 <oskargomza54@gmail.com>
2025-03-13 15:55:14 +0100

    wykład 1 proggress

--- java/advanced/w1/Main.java
+++ java/advanced/w1/Main.java
@@ -1,5 +1,37 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello World!");
+/* import java.util.*;
+    * class RandomDemo {
+    *     static int[] losuj1(int a, int b, int n) {
+    *         int[] wynik = new int[n];
+    *         Random los = new Random();
+    *         for (int i = 0; i < wynik.length; i++) {
+    *             wynik[i] = a + los.nextInt(b - a + 1);
+    *         }
+    *         return wynik;
+    *     }
+    *     public static void main(String args[]) {
+    *         Scanner reader = new Scanner(System.in);
+    *         System.out.println("Losowanie N liczb całkowitych z przedziału <a, b>");
+    *         System.out.print("Podaj a: ");
+    *         int a = reader.nextInt();
+    *         System.out.print("Podaj b: ");
+    *         int b = reader.nextInt();
+    *         System.out.print("Podaj N: ");
+    *         int n = reader.nextInt();
+    *         reader.close();
+    *         int[] tab = losuj1(a, b, n);
+    *         for (int i = 0; i < tab.length; i++) {
+    *             System.out.println(tab[i]);
+    *         }
+    *     }
+    *   }
+*/
+import javax.swing.*;
+
+class Main {
+    public static void main(String args[]) {
+        JFrame f = new JFrame("Moje pierwsze Okno");
+        f.setSize(500,500);
+        f.setVisible(true);
+    
     }
 }
